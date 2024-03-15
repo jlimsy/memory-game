bb2635cee7b3723274146242ceec918498aa490d
jlimsy <limsy.joey@gmail.com>
2024-03-15 16:17:02 +0800

    fix timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,21 +4,9 @@ export default function Timer() {
   const [seconds, setSeconds] = useState(15);
   const [isRunning, setIsRunning] = useState(false);
 
-  const handleCountdown = () => {
-    let interval;
-    if (seconds > 0) {
-      interval = setInterval(() => {
-        setSeconds((seconds) => seconds - 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-  };
+  useEffect(() => {
+    seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000);
+  }, [seconds]);
 
-  return (
-    <>
-      <button onClick={handleCountdown}>Start Game</button>
-      {seconds}
-    </>
-  );
+  return <>{seconds}</>;
 }
