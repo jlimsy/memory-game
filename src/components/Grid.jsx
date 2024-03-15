a192590e5b861ae345440ded5710dd0066513112
jlimsy <limsy.joey@gmail.com>
2024-03-15 18:36:27 +0800

    install tailwind

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,3 +1,21 @@
+const LEVEL1 = 3;
+
 export default function Grid() {
-  return <div className="cell">cell</div>;
+  let rows = [];
+  let columns = [];
+
+  for (let i = 0; i < LEVEL1; i++) {
+    rows.push(i);
+    columns.push(i);
+  }
+
+  return (
+    <>
+      {rows.map((cell, idx) => (
+        <div key={idx} className="cell">
+          cell
+        </div>
+      ))}
+    </>
+  );
 }
