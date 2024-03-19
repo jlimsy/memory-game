04267ffff5149d0932224a5b7d95a98f18450a55
jlimsy <limsy.joey@gmail.com>
2024-03-19 14:06:48 +0800

    useEffect to immediately reander state

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Cell({ id, green }) {
-  // const [selectedCells, setSelectedCells] = useState([]);
-
+export default function Cell({ id, green, selectedCells, setSelectedCells }) {
   let greenCells = green;
-  let selectedCellId;
-  const handleFlip = (event) => {
-    console.log("flip!");
-    // setSelectedCells([...selectedCells, event.target.id]);
-    selectedCellId = event.target.id;
-  };
 
-  const checkCells = () => {};
+  const handleFlip = (event) => {
+    console.log("flip:", event.target.id);
+    setSelectedCells([...selectedCells, event.target.id]);
+  };
 
   return (
     <>
-      <div
+      {/* <div
         id={`${id}`}
         className={`border border-neutral-600 h-32 w-32 rounded-lg ${
           greenCells.includes(id) ? "bg-green-400" : ""
         }`}
+      >
+        {id}
+      </div> */}
+      <div
+        id={`${id}`}
+        className="front border border-neutral-600 h-32 w-32 rounded-lg bg-green-400"
+      >
+        {id}
+      </div>
+      <div
+        id={`${id}`}
+        className="back border border-neutral-600 h-32 w-32 rounded-lg"
+        onClick={handleFlip}
       >
         {id}
       </div>
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,13 +5,13 @@ import gridSize from "../utils/gridSize";
 
 export default function Grid({ grid, show, setShow, level }) {
   const [green, setGreen] = useState([]);
+  const [selectedCells, setSelectedCells] = useState([]);
 
+  //* give each cell an id
   let numCells = [...Array(Math.pow(grid, 2))].map((cell, idx) => idx);
 
   useEffect(() => {
-    //give each cell an id
-
-    // randomly assign cell ids to be set to green
+    //* randomly assign cell ids to be set to green
     numCells.sort(() => Math.random() - 0.5);
 
     let greenCells = [];
@@ -21,13 +21,19 @@ export default function Grid({ grid, show, setShow, level }) {
 
     setGreen(greenCells);
     console.log("green", green);
-    console.log("no. green cells", gridSize[`level${level}`].green);
-  }, [level]);
+    console.log(selectedCells);
+  }, [level, selectedCells]);
 
   return (
     <div className={`grid grid-cols-${grid} gap-3`}>
       {numCells.map((cell, idx) => (
-        <Cell key={idx} id={idx} green={green} />
+        <Cell
+          key={idx}
+          id={idx}
+          green={green}
+          selectedCells={selectedCells}
+          setSelectedCells={setSelectedCells}
+        />
       ))}
     </div>
   );
